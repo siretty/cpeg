111f8851d15787f7b0d05399c65536e227ce986d
Daniel Edwards <dev@danieledwards.de>
2013-10-08 00:25:52 +0200

    fixed bugs in the cp_byteset functions

--- src/cp_byteset.h
+++ src/cp_byteset.h
@@ -11,48 +11,63 @@ struct cp_byteset {
 };
 
 
+static const struct cp_byteset cp_byteset_all = {
+  .a = 0xFFFFFFFFFFFFFFFF,
+  .b = 0xFFFFFFFFFFFFFFFF,
+  .c = 0xFFFFFFFFFFFFFFFF,
+  .d = 0xFFFFFFFFFFFFFFFF,
+};
+
+static const struct cp_byteset cp_byteset_none = {
+  .a = 0x0,
+  .b = 0x0,
+  .c = 0x0,
+  .d = 0x0,
+};
+
+
 static inline void cp_byteset_enable( struct cp_byteset *s, uint8_t b ) {
   if( b < 64 ) {
-    s->a |= 1 << b;
+    s->a |= (uint64_t)1 << b;
   }
   else if( b < 128 ) {
-    s->b |= 1 << (b - 64);
+    s->b |= (uint64_t)1 << (b - 64);
   }
   else if( b < 192 ) {
-    s->c |= 1 << (b - 128);
+    s->c |= (uint64_t)1 << (b - 128);
   }
   else {
-    s->d |= 1 << (b - 192);
+    s->d |= (uint64_t)1 << (b - 192);
   }
 }
 
 static inline void cp_byteset_disable( struct cp_byteset *s, uint8_t b ) {
   if( b < 64 ) {
-    s->a &= ! (1 << b);
+    s->a &= ~ ((uint64_t)1 << b);
   }
   else if( b < 128 ) {
-    s->b &= ! (1 << (b - 64));
+    s->b &= ~ ((uint64_t)1 << (b - 64));
   }
   else if( b < 192 ) {
-    s->c &= ! (1 << (b - 128));
+    s->c &= ~ ((uint64_t)1 << (b - 128));
   }
   else {
-    s->d &= ! (1 << (b - 192));
+    s->d &= ~ ((uint64_t)1 << (b - 192));
   }
 }
 
 static inline void cp_byteset_toggle( struct cp_byteset *s, uint8_t b ) {
   if( b < 64 ) {
-    s->a ^= 1 << b;
+    s->a ^= (uint64_t)1 << b;
   }
   else if( b < 128 ) {
-    s->b ^= 1 << (b - 64);
+    s->b ^= (uint64_t)1 << (b - 64);
   }
   else if( b < 192 ) {
-    s->c ^= 1 << (b - 128);
+    s->c ^= (uint64_t)1 << (b - 128);
   }
   else {
-    s->d ^= 1 << (b - 192);
+    s->d ^= (uint64_t)1 << (b - 192);
   }
 }
 
